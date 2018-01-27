b34ff238cd6bae41925a2d54a7522e3cc1c14ec4
Alex Klimenkov <Alex@Alexanders-MacBook-Pro-3.local>
2018-01-27 14:49:36 +0300

    add post shake (comment form)

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,9 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-    //// PUT YOUR CODE HERE
-	}
-	}, 10);
+      //// PUT YOUR CODE HERE
+    }
+  }, 10);
 });
-
-
