92a0b420db00e1b2719e0c1607540cfa1cf3fbd8
Artsiom Litvinau <flierart@gmail.com>
2018-01-27 12:06:47 +0300

    Initial commit

---
+++ src/inject/inject.js
@@ -0,0 +1,11 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+    //// PUT YOUR CODE HERE
+	}
+	}, 10);
+});
+
+
